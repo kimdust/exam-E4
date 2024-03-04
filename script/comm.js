c6c9c06c93a4a60e669f1045de105b81d09de742
kimdust <mji1223@naver.com>
2024-03-04 11:38:30 +0900

    modal

--- script/comm.js
+++ script/comm.js
@@ -7,4 +7,13 @@ $(document).ready(function () {
     now = (now + 1) % slideCount;
     slideList.css("left", 100 * -now + "%");
   }, 3000);
+
+  $(".notice ul > li:first-child").on("click", function () {
+    $(".modal").css("display", "block");
+    $(".modal-bg").css("display", "block");
+  });
+  $(".modal > button").on("click", function () {
+    $(".modal").hide();
+    $(".modal-bg").hide();
+  });
 });
